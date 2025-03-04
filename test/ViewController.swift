19f7090448abac85c1de6d1825a6996af5d4a129
neo.hoang <neo.hoang@okg.com>
2025-03-04 16:36:38 +0800

    Crash on iOS 18.4 when text contains ffi

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    let textView = UITextView(frame: .zero)
+    let textField = UITextField(frame: .zero)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textField.backgroundColor = .red
+        textField.delegate = self
+        view.addSubview(textField)
+
+        textView.font = UIFont(name: "OKXSansTAB-Regular", size: 16)
+        textView.backgroundColor = .green
+        textView.text = "ffi"
+        view.addSubview(textView)
     }
 
-
+    override func viewDidLayoutSubviews() {
+        textField.frame = CGRect(x: 0, y: 40, width: view.bounds.size.width, height: 60)
+        textView.frame = CGRect(x: 0, y: 120, width: view.bounds.size.width, height: 200)
+    }
 }
 
